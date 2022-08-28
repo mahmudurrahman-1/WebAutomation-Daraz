ca98c41e9fe686b433c6067cb0738ad02534deb5
mahmudurrahman-1 <mahmudur.rahman.sqa@gmail.com>
2022-08-29 05:23:12 +0600

    update done

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"00ce3317-ba21-4a53-a0e5-b60a481b7347","feature":"Login Module","scenario":"John open the site and Login","start":1661728763771,"group":1,"content":"","tags":"","end":1661728781423,"className":"passed"}]);
+CucumberHTML.timelineItems.pushArray([{"id":"8621fe29-286a-4238-b63d-c3e92f572711","feature":"Login Module","scenario":"John open the site and Login","start":1661728928071,"group":1,"content":"","tags":"","end":1661728945370,"className":"passed"}]);
 CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
 });
