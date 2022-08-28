cca1f6a3980dc431c0bdf6369d5f246c5e8d1f29
mahmudurrahman-1 <mahmudur.rahman.sqa@gmail.com>
2022-08-29 05:16:04 +0600

    test runner updated

--- src/test/java/testrunner/Boom.java
+++ src/test/java/testrunner/Boom.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/java/feature/registration.feature",
+        features = "src/test/java/feature/login.feature",
         glue = "stepdef",
         plugin = {"pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                "timeline:test-output-thread/", "rerun:target/failedrerun.txt"}
